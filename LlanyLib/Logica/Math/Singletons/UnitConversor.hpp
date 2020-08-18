1e1f23f11a0bececf08612260ad28df5971775fd
Francisco Julio <Llanyro@gmail.com>
2020-08-18 08:41:20 +0200

    Update UnitConversor.hpp

--- LlanyLib/Logica/Math/Singletons/UnitConversor.hpp
+++ LlanyLib/Logica/Math/Singletons/UnitConversor.hpp
@@ -1,1 +1,31 @@
-#
+#pragma once
+#ifndef UNIT_CONVERSOR_SINGLETON_HPP_
+#define UNIT_CONVERSOR_SINGLETON_HPP_
+
+#include "../../Basic/Plantillas/Otros/Singleton.hpp"
+#include "../../Basic/Gestores/SubscriptorSingletons.hpp"
+
+#define MATH LlanyLib::Math::Singletons::UnitConversor::getInstancia()
+
+namespace LlanyLib {
+	namespace Math {
+		namespace Singletons {
+			class UnitConversor : public Basic::Templates::Singleton<Math>, Basic::Gestores::SubscriptorSingletons
+			{
+				#pragma region Singleton
+				private:
+					friend class Singleton<UnitConversor>;
+					UnitConversor();
+					~UnitConversor();
+				#pragma endregion
+				public:
+				void toBinary() const;
+				void toBinary() const;
+				void toBinary() const;
+
+				void toBinary() const;
+			};
+		}
+	}
+}
+#endif // !UNIT_CONVERSOR_SINGLETON_HPP_
