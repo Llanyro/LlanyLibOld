6a5cdc4a7af4d6d8f26bb4417e0fab071eca6eea
Llanyro <Llanyro@gmail.com>
2020-09-25 01:12:17 +0200

    fgjnfcvhgb

--- LlanyLib/Logica/Juego/Laberintos/Celda.hpp
+++ LlanyLib/Logica/Juego/Laberintos/Celda.hpp
@@ -20,21 +20,29 @@ namespace LlanyLib {
 				Basic::Templates::Listas::Array<bool>* posiciones;
 				Basic::Templates::Listas::LinkedList<Celda*>* grupo;
 			public:
-				Celda(const size_t& numPos);
+				#pragma region Constructores
+				Celda();
+				Celda(const size_t& numPos);				
 				Celda(const Celda& other);
 				bool operator=(const Celda& other);
 				~Celda();
-
+				#pragma endregion
+				#pragma region Getters
 				const Basic::Templates::Listas::LinkedList<Celda*>* getGrupo() const;
-
-				void setAll(const bool& value);
-
+				const bool* getValue(const size_t& posicion) const;
+				#pragma endregion
+				#pragma region Setters
+				void setAll(const bool& value) const;
+				bool set(const bool& value, const size_t& posicion) const;
+				#pragma endregion
+				#pragma region Compares
 				bool igual(const Celda& other) const;
 				//bool similar(const Critico& other) const;
-
+				#pragma endregion
+				#pragma region Operators
 				bool operator==(const Celda& other) const;
 				bool operator!=(const Celda& other) const;
-
+				#pragma endregion
 				void mergeGrupos(const Celda& other);
 				bool contiene(const Celda* other) const;
 				void romperGrupo();
